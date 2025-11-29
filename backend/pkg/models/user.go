14df5b7d486e320281e4ddb373a54abdc75f2902
Ales Shin <shinshin.a.a@edu.mirea.ru>
2025-11-29 15:36:25 +0300

    Update user.go

--- backend/pkg/models/user.go
+++ backend/pkg/models/user.go
@@ -1,14 +1,26 @@
 package models
 
-import (
-	"time"
-)
-
 type User struct {
-	ID        uint
-	UserName  string    `gorm:"not null;unique"`
-	Email     string    `gorm:"not null;unique"`
-	Password  string    `gorm:"not null"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+    ID        uint
+    UserName  string `gorm:"not null;unique"`
+    Email     string `gorm:"not null;unique"`
+    Password  string `gorm:"not null"`
+    CreatedAt time.Time
+    UpdatedAt time.Time
 }
+
+type Project struct {
+    ID        uint
+    Name      string `gorm:"not null"`
+    CreatedBy uint
+    // отношения
+}
+
+type Review struct {
+    ID        uint
+    Text      string `gorm:"unique;not null"`
+    Rating    int
+    JSONData  string
+    // остальные поля
+}
+
