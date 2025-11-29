7adc0ba63188c6294ec5b9ce681ebcd15d653b32
Полянский <polyansky12aleksey@gmail.com>
2025-11-29 22:04:10 +0300

    Starting commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import ActiveProjects from './pages/ActiveProjects';
-import ArchiveProjects from './pages/ArchiveProjects';
-import Profile from './pages/Profile';
-import './App.css';
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
+import ProfilePage from './ProfilePage';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/active" element={<ActiveProjects />} />
-            <Route path="/archive" element={<ArchiveProjects />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </Router>
   );
 }
