5bb07eda4f7fb246ddf39e68610de1540c521b6c
Ales Shin <shinshin.a.a@edu.mirea.ru>
2025-11-30 00:38:02 +0300

    Update review.go

--- backend/pkg/models/review.go
+++ backend/pkg/models/review.go
@@ -1,11 +1,20 @@
 package models
 
+import (
+    "gorm.io/gorm"
+    "time"
+)
+
+// Review структура для хранения отзывов
 type Review struct {
-	ID           uint
-	Text         string `gorm:"unique;not null"`
-	Source       string
-	JSONData     string
-	RatingSource int `gorm: foreign-key` // FIXME: исправить, написал наобум
-	TeacherRate  int
-	ModelRate    int
+    ID         uint
+    Text       string `gorm:"unique;not null"`
+    Source     string
+    JSONData   string
+    ProjectName string
+    CreatedAt  time.Time
+    UpdatedAt  time.Time
+    RatingSource int `gorm:"foreignKey"`
+    TeacherRate int
+    ModelRate  int
 }
