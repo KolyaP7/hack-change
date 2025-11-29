1fdf826b1dc1b65ac46db34fca130172c21503da
Ales Shin <shinshin.a.a@edu.mirea.ru>
2025-11-29 15:42:50 +0300

    Update project.go

--- backend/pkg/models/project.go
+++ backend/pkg/models/project.go
@@ -1,1 +1,10 @@
+package models
+
+type Project struct {
+    ID        uint
+    Name      string `gorm:"not null"`
+    CreatedBy uint   `gorm:"not null"`
+    CreatedAt time.Time
+    UpdatedAt time.Time
+}
 
